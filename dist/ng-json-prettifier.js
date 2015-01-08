e9b9fe4f8ae673b9634080c300e55b888bae07dc
Arnaud Drizard <arnaud.drizard@gmail.com>
2015-01-08 12:56:56 +0100

    build process

--- dist/ng-json-prettifier.js
+++ dist/ng-json-prettifier.js
@@ -3,58 +3,56 @@ if (typeof module !== "undefined" && typeof exports !== "undefined" && module.ex
 }
 
 (function (window, angular, undefined) {
-  /*jshint globalstrict:true*/
-  /*global angular:false, window: true*/
-  /*jshint unused:false*/
-  'use strict';
+/* global angular:false */
+'use strict';
 
-  angular.module('pretty.json', [])
-    .directive('prettyJson',  ['$compile', function($compile) {
-      return {
-        restrict: 'E',
-        replace: true,
-        template: '<pre></pre>',
-        scope: {
-          json: '=json',
-          replaceKey: '=replaceKey',
-          replaceValue: '=replaceValue',
-          indentValue: '=',
-          indentKey: '='
-        },
-        link: function(scope, element, attrs) {
-          var replaceKey   = scope.replaceKey || function(key, value) { return key };
-          var replaceValue = scope.replaceValue || function(key, value) { return value };
-          var indentValue  = scope.indentValue || '    ';
-          var indentKey    = scope.indentKey || 10;
+angular.module('pretty.json', [])
+  .directive('prettyJson',  ['$compile', function($compile) {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<pre></pre>',
+      scope: {
+        json: '=json',
+        replaceKey: '=replaceKey',
+        replaceValue: '=replaceValue',
+        indentValue: '=',
+        indentKey: '='
+      },
+      link: function(scope, element, attrs) {
+        scope.$watch(
+          function() { return scope.json; },
+          compileJSON
+        )
 
-          scope.$watch(
-            function() { return scope.json; },
-            compileJSON
-          )
+        var replaceKey   = scope.replaceKey || function(key, value) { return key };
+        var replaceValue = scope.replaceValue || function(key, value) { return value };
+        var indentValue  = scope.indentValue || '    ';
+        var indentKey    = scope.indentKey || 10;
 
-          function compileJSON(json) {
-            if (!json || typeof json !== 'object')
-              return;
+        function compileJSON(json) {
+          if (!json || typeof json !== 'object')
+            return;
 
-            element.html(JSON.stringify(json, prettifyJSON, indentKey).replace(new RegExp('":', 'g'),'":' + indentValue + ''))
-            $compile(element.contents())(scope);
-          }
+          element.html(JSON.stringify(json, prettifyJSON, indentKey).replace(new RegExp('":', 'g'),'":' + indentValue + ''))
+          $compile(element.contents())(scope);
+        }
 
-          function prettifyJSON(key, value) {
-            if (value && typeof value === 'object') {
-              var replacement = {};
-              for (var k in value) {
-                if (Object.hasOwnProperty.call(value, k)) {
-                  replacement[replaceKey(k, value[k])] = value[k];
-                }
+        function prettifyJSON(key, value) {
+          if (value && typeof value === 'object') {
+            var replacement = {};
+            for (var k in value) {
+              if (Object.hasOwnProperty.call(value, k)) {
+                replacement[replaceKey(k, value[k])] = value[k];
               }
-              return replacement
             }
-
-            return replaceValue(key, value);
+            return replacement
           }
+
+          return replaceValue(key, value);
         }
       }
-    }]);
+    }
+  }]);
 
 })(window, window.angular);
